4c2f3e1ec4df4aaf9db760c4e4cc2740456bb223
xvekii <vedran.dautanec@gmail.com>
2025-05-28 23:39:39 +0200

    Add the home and menu btns event listeners

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import "./styles.css";
 import marioImg from "./assets/images/mario.png";
 const content = document.getElementById("content");
 import { createHome } from "./home-tab.js";
+import { addMeals } from "./menu-tab.js";
+const menuBtn = document.querySelector(".menu-btn");
+const homeBtn = document.querySelector(".home-btn");
 
 const imgContainer = document.querySelector(".img-div-right");
 const mainImg = document.createElement("img");
@@ -10,6 +13,25 @@ mainImg.classList.add("background-img");
 
 imgContainer.appendChild(mainImg);
 
-const newHomeContainer = createHome();
-content.appendChild(newHomeContainer);
+function renderInitialHomeTab() {
+  const newHomeContainer = createHome();
+  content.appendChild(newHomeContainer);
+}
 
+function clearContent() {
+  content.replaceChildren();
+}
+
+homeBtn.addEventListener("click", () => {
+  clearContent();
+  const newHomeContainer = createHome();
+  content.appendChild(newHomeContainer);
+});
+
+menuBtn.addEventListener("click", () => {
+  clearContent();
+  const newMenuContainer = addMeals();
+  content.appendChild(newMenuContainer);
+});
+
+renderInitialHomeTab();
